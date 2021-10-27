aa762dec2cc460a2d76d8aefc9d33dbed7fdf77c
Muskan2905 <69806508+Muskan2905@users.noreply.github.com>
2021-10-27 14:13:39 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,9 @@
 // Tag Selectors
 const stickyNav = document.querySelector("nav");
 const togglerButton = document.querySelector(".check-togglerbtn");
-// const navItems = document.querySelector(".nav-ul");
+const dropdown = document.querySelector(".dropdown")
+const dropdownContent = document.querySelector(".dropdown-content")
+const margin = document.querySelector(".regbtn");
 
 window.addEventListener("scroll",()=>{
 stickyNav.classList.toggle("sticky",window.scrollY > 0);
@@ -12,3 +14,22 @@ togglerButton.addEventListener("click", function () {
     togglerButton.classList.toggle("toggle-icon");
     background.classList.toggle("hide-background");
 });
+
+dropdown.addEventListener("click",function(){
+    dropdownContent.classList.toggle("show");
+    margin.classList.toggle("margin");
+});
+
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.querySelector(".dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
